f852314ef00fae0f4d01e44b24a7fcafa709c04b
Gozali Kumara <ghk@gozalikumara.com>
2012-06-16 04:10:13 +0700

    schema api baru

--- borobudur/schema.py
+++ borobudur/schema.py
@@ -1,47 +1,87 @@
 import colander
 from colander import null
-from bson.dbref import DBRef
-from bson.objectid import ObjectId
-import operator
 
 class Ref(object):
     def serialize(self, node, appstruct):
+        from bson.dbref import DBRef
+        from bson.objectid import ObjectId
         if appstruct is None:
             return null
         if not isinstance(appstruct, DBRef):
-            raise Invalid(node, 'input is not DBRef')
+            raise colander.Invalid(node, 'input is not DBRef')
         return dict(collection=appstruct.collection, id=str(appstruct.id))
 
     def deserialize(self, node, cstruct):
+        from bson.dbref import DBRef
+        from bson.objectid import ObjectId
         if cstruct is null:
             return None
         if not isinstance(cstruct, dict):
-            raise Invalid(node, 'input is not a dict')
-        elif ((cstruct.get("collection") is None) or (cstruct.get("id") is None)):
-            raise Invalid(node, 'input is not a correct reference')
+            raise colander.Invalid(node, 'input is not a dict')
+        elif (cstruct.get("collection") is None) or (cstruct.get("id") is None):
+            raise colander.Invalid(node, 'input is not a correct reference')
         result = DBRef(collection=cstruct.get("collection"), id=ObjectId(cstruct.get("id")))
         return result
 
-class SchemaRepository(object):
+def create_schema_node_from_cls(cls):
+    """
+    prambanan:type cls c(object)
+    """
+    schema = colander.SchemaNode(colander.Mapping())
+    schema.schema_name = ""
+    obj = cls()
+    attrs = dir(obj)
+    for key in attrs:
+        if not key.startswith("__"):
+            value = getattr(obj, key)
+            if isinstance(value, colander.SchemaNode):
+                child = value.clone()
+                child.name = key
+                schema.add(child)
+    return schema
 
+class SchemaRepository(object):
     def __init__(self):
         self.schemas = {}
 
     def register(self, schema):
-        key = schema.schema_namespace if (schema.schema_name == "") else ("%s.%s" % (schema.schema_namespace, schema.schema_name))
+        key = schema.schema_namespace if (schema.schema_name == "") else (
+        "%s.%s" % (schema.schema_namespace, schema.schema_name))
         self.schemas[key] = schema
         return schema
 
-    def get(self, schema_namespace, schema_name = ""):
+    def get(self, schema_namespace, schema_name=""):
         key = schema_namespace if (schema_name == "") else ("%s.%s" % (schema_namespace, schema_name))
         return self.schemas[key]
+
+    def root(self, schema_namespace):
+
+        def decorate(cls):
+            schema = create_schema_node_from_cls(cls)
+            schema.schema_namespace = schema_namespace
+            schema.schema_name = ""
+            self.register(schema)
+            return cls
+
+        return decorate
+
+    def child(self, schema_namespace, schema_name):
+
+        def decorate(cls):
+            schema = create_schema_node_from_cls(cls)
+            schema.schema_namespace = schema_namespace
+            schema.schema_name = schema_name
+            self.register(schema)
+            return cls
+
+        return decorate
 
     def add_mapping(self, schema_namespace, nodes):
         schema = colander.SchemaNode(colander.Mapping())
         schema.schema_name = ""
         schema.schema_namespace = schema_namespace
 
-        for key, child in sorted(nodes.items(), key=lambda (k,v):  v._order):
+        for key, child in sorted(nodes.items(), key=lambda (k, v): v._order):
             child = child.clone()
             child.name = key
             schema.add(child)
@@ -97,13 +137,15 @@ class SchemaRepository(object):
 
         return self.register(schema)
 
+
 def anonymous_mapping(nodes):
     schema = colander.SchemaNode(colander.Mapping())
-    for key, child in sorted(nodes.items(), key=lambda (k,v):  v._order):
+    for key, child in sorted(nodes.items(), key=lambda (k, v): v._order):
         child = child.clone()
         child.name = key
         schema.add(child)
     return schema
+
 
 def anonymous_sequence(child):
     schema = colander.SchemaNode(colander.Sequence())
