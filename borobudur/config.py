c628a010fa3f0910d050514e1a585e4c09b4964e
Gozali Kumara <ghk@gozalikumara.com>
2012-10-14 03:58:13 +0700

    move all resources thingy to resources

--- borobudur/config.py
+++ borobudur/config.py
@@ -1,11 +1,8 @@
 from pyramid.httpexceptions import HTTPFound
 from pyramid.response import Response
-from pyramid.renderers import render_to_response
 
 import borobudur
 from borobudur.interfaces import IAppConfigurator, IBootstrapSubscriber, IAppRoot, IAssetCalculator
-import borobudur.resource.storage
-import borobudur.resource.storage.mongo
 
 from borobudur.asset import SimplePackCalculator
 from borobudur.model import Model
@@ -42,62 +39,12 @@ def wrap_pyramid_view(handler_type_id):
 
 
 
-def expose(*exposers):
-    def decorate(cls):
-        setattr(cls, "_exposers", exposers)
-        return cls
-
-    return decorate
-
-
-def try_expose(cls, config, app, factory):
-    if hasattr(cls, "_exposers"):
-        for exposer in getattr(cls, "_exposers"):
-            exposer(config, app, factory, cls)
-
-
 def create_factory(**kwargs):
     class Factory(object):
         def __init__(self, request):
             self.__dict__.update(kwargs)
 
     return Factory
-
-
-class AppResources(object):
-    def __init__(self, request, resource_types, storage_type_map, service_id_map):
-        self.request = request
-        self.resource_types = resource_types
-        self.storage_type_map = storage_type_map
-        self.service_id_map = service_id_map
-
-    def get_storage(self, model):
-        storage_type = self.storage_type_map.get(model, None)
-        if storage_type is None:
-            return None
-        return storage_type(self.request)
-
-    def get_service(self, id):
-        service_type = self.service_id_map[id]
-        return service_type(self.request)
-
-r_map = {}
-
-def add_resources(config, name, resource_types, root):
-    factory = create_factory(resources_name=name)
-
-    storage_type_map = {}
-    service_id_map = {}
-    for resource_type in resource_types:
-        if issubclass(resource_type, borobudur.resource.storage.mongo.MongoStorage) or issubclass(resource_type,
-            borobudur.resource.storage.mongo.EmbeddedMongoStorage):
-            storage_type_map[resource_type.model] = resource_type
-        if hasattr(resource_type, "id"):
-            service_id_map[resource_type.id] = resource_type
-
-    r_map[name] = (storage_type_map, service_id_map, resource_types, root)
-    for resource_type in resource_types:
-        try_expose(resource_type, config, root, factory)
 
 
 def add_borobudur(config, name, app_config, root="/" ):
@@ -116,11 +63,6 @@ def add_borobudur(config, name, app_config, root="/" ):
     asset_calculator = app_config.asset_calculator_factory(app_config, name, config.registry)
     config.registry.registerUtility(asset_calculator, IAssetCalculator, name=name)
     config.registry.registerUtility(app_config, IAppConfigurator, name=name)
-
-def get_resources(request):
-    storage_type_map, service_id_map, resource_types, resource_root = r_map[request.context.resources_name]
-    app_resources = AppResources(request, resource_types=resource_types, storage_type_map=storage_type_map, service_id_map=service_id_map)
-    return app_resources
 
 
 def get_app(request):
@@ -147,10 +89,8 @@ def add_global_bootstrap_subscriber(config, subscriber_qname):
 
 def includeme(config):
     config.add_directive('add_borobudur', add_borobudur)
-    config.add_directive('add_resources', add_resources)
     config.add_directive('add_global_bootstrap_subscriber', add_global_bootstrap_subscriber)
 
-    config.set_request_property(get_resources, 'resources', reify=True)
     config.set_request_property(get_app, 'app', reify=True)
     config.set_request_property(get_app_config, 'app_config', reify=True)
     config.set_request_property(get_document, 'document', reify=True)
